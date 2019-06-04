4201258e3b38ddb951febf2246f5a7f021837e9c
Nick Wesselman <nwe@sitecore.net>
2019-06-04 14:45:34 -0400

    [#88] Ensure TemplateWizard always has 1.0.0.0 version
    * Set up shared assembly info between Rocks assemblies, and one shared assembly version which Appveyor can update
    * Create new globals solution folder for these and other items that were in 'docs'

---
+++ src/SharedAssemblyVersion.cs
@@ -0,0 +1,9 @@
+﻿// © 2015-2016 Sitecore Corporation A/S. All rights reserved.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// These will be populated by AppVeyor
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/Sitecore.Rocks/Properties/AssemblyInfo.cs
+++ src/Sitecore.Rocks/Properties/AssemblyInfo.cs
@@ -1,19 +1,7 @@
 ﻿// © 2015-2016 Sitecore Corporation A/S. All rights reserved.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle(@"Sitecore.Rocks")]
-[assembly: AssemblyDescription(@"Sitecore Rocks makes Sitecore developers happy. Directly integrated into Microsoft Visual Studio, Sitecore Rocks provides a fast and streamlined development experience with tools that developers are used to.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany(@"Sitecore")]
-[assembly: AssemblyProduct(@"Sitecore.Rocks")]
-[assembly: AssemblyCopyright(@"Copyright © Sitecore A/S 2010-2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: Guid(@"258cc76c-43ca-4201-9059-76f170bdc9ab")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Sitecore.Rocks.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100E33008B17797834BA1F9D494BB026A0B73D8B39564C1648883A0F25D3EB8B901FC98F27B3A1041258A826C7DCEC2E428D1DB93486592575D302F06796B5E21B423179666F5C8F562134D4861CB708431FB28876BBE6C18A1A8B39CCBEAAFF670BBE6C58A7E102EB57C5A84BCFF7C4004DB87FF7D11839CBE23E21A7F2AE7E79C")]
