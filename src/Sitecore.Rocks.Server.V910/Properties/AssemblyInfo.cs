5b8dd3942c60d22b0e570bf014b9ee81c9ef3b2a
Nick Wesselman <nwe@sitecore.net>
2019-07-22 15:47:36 -0400

    Fix versioning of version specific assemblies

--- src/Sitecore.Rocks.Server.V910/Properties/AssemblyInfo.cs
+++ src/Sitecore.Rocks.Server.V910/Properties/AssemblyInfo.cs
@@ -1,16 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Sitecore.Rocks.Server.V910")]
-[assembly: AssemblyDescription(@"Sitecore Rocks makes Sitecore developers happy. Directly integrated into Microsoft Visual Studio 2015/2017, Sitecore Rocks provides a fast and streamlined development experience with tools that developers are used to.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany(@"Sitecore")]
-[assembly: AssemblyProduct(@"Sitecore.Rocks")]
-[assembly: AssemblyCopyright(@"Copyright © Sitecore A/S 2010-2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: Guid("ac426fec-50f1-4250-b915-b22888a0eee5")]
-[assembly: AssemblyVersion(@"2.1.0.0")]
-[assembly: AssemblyFileVersion(@"2.1.88")]
